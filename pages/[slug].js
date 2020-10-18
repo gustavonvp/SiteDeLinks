13bebb4f5e2d27268217cc01001183e6fe746b4d
gustavonvp <gustavonvp@gmail.com>
2020-10-18 20:32:24 -0300

    - node_modules, lock, .next , .....

---
+++ pages/[slug].js
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react'
+import Prismic from 'prismic-javascript'
+import {useRouter} from 'next/router'
+import Head from 'next/head'
+
+const RedirectTo = () =>{
+        const router = useRouter()
+        useEffect(()=>{
+            setTimeout(()=>{
+                router.push("/")
+            },2000)
+        },[])
+        
+    return (
+        <div className="font-bold mx-auto text-center mt-4">
+            <Head>
+                <title>Página não encontrada</title>
+            </Head>
+            <h1 className="font-bold text-4xl" >URL não encontrada</h1>
+            <p>Estamos redirecionando você para uma página  central de links</p>
+        </div>
+
+    )
+}
+
+export async function getServerSideProps({params, res}){
+    console.log(params)
+    const client = Prismic.client("https://gustavonunes.cdn.prismic.io/api/v2");
+    const link = await client.getByUID('shortlink',params.slug);
+    if(link){
+        res.statusCode= 301 //conteudo removido permanentemente
+        res.setHeader('Location',link.data.destino.url)//redireciona
+        res.end();
+        return;
+    }
+    console.log(link)
+    return {
+        props:{},
+    }
+}
+
+
+export default RedirectTo;
---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Prismic from 'prismic-javascript'
+import Head from 'next/head'
+
+
+const Index = ({data}) => {
+    console.log("client");
+    return (
+        <div 
+        style={{ 
+            backgroundColor: data.cor_de_fundo, 
+            color: data.cor_de_texto 
+            }}
+        >
+                 <Head>
+                    <title>{data.pagetitle}</title>
+                </Head>    
+        <div className="w-1/2 mx-auto text-center">
+        <h1 className="text-4xl font-bold p-8">{data.titulo}</h1>
+            <img className="mx-auto rounded-full shadow-2xl w-1/4" src={data.logo.url} />   
+                <ul>
+                    {data.body.map((item) =>{
+                        if(item.slice_type === 'secao'){
+                            return <h2 className="font-bold text-2xl p-8 pt-4">{item.primary.nome}</h2>
+                        }
+                        
+                        if(item.slice_type === 'link'){
+                            return (
+                                <div>
+                                    <a 
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 inline-block"
+                                    href={item.primary.destino.url}>
+                                        {item.primary.texto_do_botao}
+                                    </a>
+
+                                </div>
+                            );
+                       
+                        }
+
+                        if(item.slice_type === 'imagem'){
+                            return(
+                                <img src={item.primary.imagem.url} className="mx-auto"/>
+                            )
+                        }
+                       
+                       return null;   
+                    })}
+                </ul>    
+            </div>        
+            <div className="text-center py-4 ">
+                    Projeto criado durante o evento Dev10k <a href="https://devpleno.com">Modificado</a>
+                    <br />
+                    Codigo Fonte disponível em: ...
+            </div>                        
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    console.log("server");
+    const client = Prismic.client("https://gustavonunes.cdn.prismic.io/api/v2");
+    const centralLinks = await client.getSingle("centrallinks")
+    console.log(centralLinks);
+    return {
+        props: {
+            data: centralLinks.data
+        }
+    }
+}
+
+
+export default Index;

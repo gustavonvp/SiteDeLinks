acb76c306ee3b8fd6aa0cad83e8d1a85c41a2a4f
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-07-11 17:05:28 -0300

    update vercel  Closes 14

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  poweredByHeader: false,
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };

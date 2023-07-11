7578971c1a84f726a55b1e2826df198d81b6f76a
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-07-11 17:58:13 -0300

    update lock json Closes #17

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   poweredByHeader: false,
+  productionBrowserSourceMaps: true,
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };

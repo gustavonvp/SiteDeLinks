5430b801764a15951f956639c3e55f3a28f30b2d
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-07-11 16:29:01 -0300

    page properties Closes #13

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    webpack5: true,
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false };
-  
-      return config;
-    },
-  };
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};

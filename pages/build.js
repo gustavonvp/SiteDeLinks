bf3398df9243f7356c99698accbb7e33321a616d
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-07-11 16:16:55 -0300

    Update build.js

--- pages/build.js
+++ pages/build.js
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs';
+import { writeFile } from 'fs/promises';
 writeFile('../../built-time.js', `module.exports = '${new Date()}'`, (err) => {
   if (err) throw err;
   console.log('Build time file created successfully!');

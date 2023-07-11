03cc73a083465e20597470b195b628da6192e12b
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-07-11 19:50:03 -0300

    NO using builder Closes #23

--- pages/build.js
+++
@@ -1,5 +0,0 @@
-const Write = require('fs/promises')
-Write.writeFile('../../built-time.js', `module.exports = '${new Date()}'`, (err) => {
-  if (err) throw err;
-  console.log('Build time file created successfully!');
-});
